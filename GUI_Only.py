ff3863c4911461ddaab20db29549081e771d6c59
annmonrajijames <annmonjameschiramel@gmail.com>
2024-05-10 10:31:11 +0530

    Setting up window

---
+++ GUI_Only.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+
+# Set the title of the window
+root.title("HIL")
+
+# Set the size of the window
+root.geometry("1000x600")
+
+# Start the event loop
+root.mainloop()
